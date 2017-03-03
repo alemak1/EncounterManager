069b89b25771a1088cf92257d0c7b628b6b579e7
yongrenwang <czie.alexmak@gmail.com>
2017-03-03 22:07:43 +0800

    Added Encounter.sks files to a separate folders; created a basic stub for an EncounterManager class

---
+++ Pierre Penguin Escapes the Antarctic/EncounterManager.swift
@@ -0,0 +1,23 @@
+//
+//  EncounterManager.swift
+//  Pierre Penguin Escapes the Antarctic
+//
+//  Created by Aleksander Makedonski on 3/3/17.
+//  Copyright © 2017 JoyfulGames.io. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+
+class EncounterManager{
+    
+    let encounterName: [String] = [
+        "EncounterBats",
+        "EncounterBees",
+        "EncounterMadFlies"
+    ]
+    
+    let encounters: [SKNode] = []
+
+}
